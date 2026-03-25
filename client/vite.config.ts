be8a9488d268810196910b5dd30bbd17eaef378d
Vijay Rachumallu <vijayrachumallu45@gmail.com>
2026-03-25 21:18:59 +0530

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-
+import path from "path";
 
 
 export default defineConfig({
@@ -9,4 +9,9 @@ export default defineConfig({
     react(),
     tailwindcss(),
     ],
+  resolve: {
+    alias: {
+      '@' : path.resolve(__dirname, './src'),
+    }'
+  },
 });
