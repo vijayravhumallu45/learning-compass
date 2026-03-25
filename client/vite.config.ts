6699b6f3c988bc8a9f14d307ae4c9c0c2a75a825
Vijay Rachumallu <vijayrachumallu45@gmail.com>
2026-03-25 21:04:30 +0530

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,16 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import path from "path";
-import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
-import { metaImagesPlugin } from "./vite-plugin-meta-images";
+
 
 
 export default defineConfig({
   plugins: [
     react(),
-    runtimeErrorOverlay(),
-    metaImagesPlugin(),
     tailwindcss(),
     ],
 });
