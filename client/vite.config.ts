348b47fb2691400599c5b41a1785466fa1f285e1
Vijay Rachumallu <vijayrachumallu45@gmail.com>
2026-03-25 20:48:25 +0530

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   plugins: [
     react(),
     runtimeErrorOverlay(),
-    metaImagesPlugin()
+    metaImagesPlugin(),
     tailwindcss(),
     
     ...(process.env.NODE_ENV !== "production" &&
